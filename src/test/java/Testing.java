b8cfbe3c39ceaad115b2d1f18d989206538c6319
hajin <68993476+hkim532@users.noreply.github.com>
2020-10-22 09:10:09 +1300

    just testing out maven + CI

---
+++ src/test/java/Testing.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class Testing {
+
+	@Test
+	public void test() {
+		assertEquals("hello world", Main.helloWorld());
+	}
+
+}
